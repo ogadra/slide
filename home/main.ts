4e1869719407b4f6f4249d9e074e3ba2c87832d9
isucon server <61941819+ogadra@users.noreply.github.com>
2025-03-12 22:03:01 +0900

    🏗 Set up

---
+++ home/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+import 'uno.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1 class="m-0">ogadra's slides</h1>
+  <div class="flex flex-col">
+    // ここにスライドを追加していく
+  </div>
+`
