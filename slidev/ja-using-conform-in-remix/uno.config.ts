a20bf8b463ce44f36227c5871aeeda77307a5484
isucon server <61941819+ogadra@users.noreply.github.com>
2025-03-28 21:54:06 +0900

    💄 Update background color

--- slidev/ja-using-conform-in-remix/uno.config.ts
+++ slidev/ja-using-conform-in-remix/uno.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
   },
   shortcuts: {
     // custom the default background
-    // 'bg-main': 'bg-[#1E1E1E] text-[#D4D4D4]',
-    // 'bg-main': 'bg-[#3e3e3e] text-[#fafafa]',
+    'bg-main': 'bg-[#1E1E1E] text-[#D4D4D4]',
     'border-image': 'border border-[#121212] border-opacity-10 shadow-md shadow-[#121212]',
     'bg-barBottom': 'bg-[#007ACC] text-[#FFFFFF]',
     'bg-barBottomLeft': 'bg-[#16825D] text-[#FFFFFF]',
