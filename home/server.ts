93dc18375024d7d7eac33565ff6602d9a423891a
isucon server <61941819+ogadra@users.noreply.github.com>
2025-03-29 21:33:50 +0900

    ✨ Add ogp for top

--- home/server.ts
+++ home/server.ts
@@ -1,21 +1,15 @@
 import { type Context, Hono } from "hono";
-import { rewriter, ElementHandler } from "./htmlRewriter";
+import { HTMLRewriterHandler } from "./htmlRewriterHandler";
 
 const app = new Hono();
 
-app.on("GET", ["/:slide/:num"], async (c: Context) => {
-  const regex = /^(https?:\/\/[^/]+\/[^/]+\/)/;
-  const urlPrefix = c.req.url.match(regex)?.[1] ?? c.req.url;
-  const html = await c.env.ASSETS.fetch(urlPrefix);
-
-  return rewriter
-    .on(
-      "head",
-      new ElementHandler(`${urlPrefix}slides-export/${c.req.param("num")}.png`)
-    )
-    .transform(html);
+app.on("GET", ["/:slide"], async (c: Context) => {
+  return HTMLRewriterHandler(c, 1);
 });
 
+app.on("GET", ["/:slide/:num"], async (c: Context) => {
+  return HTMLRewriterHandler(c, Number(c.req.param("num")));
+});
 
 app.on("GET", ["*"], async (c: Context) => {
   return c.env.ASSETS.fetch(c.req.url);
