7d64d80adab0515bc580eff560d5ec6fb8b3a300
isucon server <61941819+ogadra@users.noreply.github.com>
2025-06-23 09:31:49 +0900

    📝 Add upgrade to rrv7

---
+++ slidev/upgrade-to-rrv7/components/Footer.vue
@@ -0,0 +1,31 @@
+<style scoped>
+div {
+  font-size: 0.8rem;
+}
+</style>
+
+<template>
+  <div class="fixed left-0 bottom-0 bg-barBottom flex absolute w-full text-sm">
+    <div class="text-left bg-barBottomLeft left-0 py-0.5 px-2">
+      hono-remix-adapter使ってみた
+    </div>
+
+    <div class="w-1/2 flex justify-end ml-auto px-2">
+      <div class="flex items-center mr-1">
+        <carbon:logo-github />
+
+        <div class="mx-1">ogadra</div>
+      </div>
+
+      <div class="flex items-center mr-2">
+        <carbon:logo-twitter />
+        <div class="mx-1">const_myself</div>
+      </div>
+
+      <div class="flex items-center">
+        <carbon:link />
+        <div class="ml-1">slide.ogadra.com</div>
+      </div>
+    </div>
+  </div>
+</template>
