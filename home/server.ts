26001954e8bbfc659f6946f45ba02d87584945db
ogadra <61941819+ogadra@users.noreply.github.com>
2025-08-23 23:40:54 +0900

    migrate: index.html -> app/index.tsx

--- home/server.ts
+++ home/server.ts
@@ -8,12 +8,12 @@ app.on("GET", ["/assets/*"], async (c: Context) => {
   return c.env.ASSETS.fetch(c.req.url);
 });
 
-app.on(["GET", "POST"], ["/demo/*"], async (c: Context) => {
-  return Index(c);
-});
-
 app.on("GET", ["/:slide", "/:slide/", "/:slide/:num"], async (c: Context) => {
   return HTMLRewriterHandler(c, Number(c.req.param("num") ?? 1));
+});
+
+app.on("GET", ["/"], async (c: Context) => {
+  return Index(c);
 });
 
 app.on("GET", ["*"], async (c: Context) => {
