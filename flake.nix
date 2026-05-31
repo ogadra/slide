d73e24f677ef78dedfcf2eea269d6a7f9f93e3a7
ogadra <61941819+ogadra@users.noreply.github.com>
2026-05-31 13:07:42 +0900

    provide pnpm via nixpkgs in flake devShell

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_24
-            corepack
+            pnpm
             google-chrome
           ];
 
